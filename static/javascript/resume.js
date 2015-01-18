915a437b36aab83953e121167755c154d70c2dd3
J-east <jakepevans@gmail.com>
2015-01-18 16:28:34 -0500

    added jquery to detect when div is in view and change the color of the navbar

--- static/javascript/resume.js
+++ static/javascript/resume.js
@@ -8,7 +8,7 @@ $('#navbar-affix').affix({
 })
 
 $(function() {
-  $('a[href*=profile]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -21,4 +21,49 @@ $(function() {
     }
   });
 });
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function() {
+		if (isScrolledIntoView('#profile')){
+			$('#profile-but').addClass('active');
+			$('#work-but').removeClass('active');
+			$('#abilities-but').removeClass('active');
+			$('#projects-but').removeClass('active');
+		}
+		if (isScrolledIntoView('#work')){
+			$('#profile-but').removeClass('active');
+			$('#work-but').addClass('active');
+			$('#abilities-but').removeClass('active');
+			$('#projects-but').removeClass('active');
+		}
+		if (isScrolledIntoView('#abilities')){
+			$('#profile-but').removeClass('active');
+			$('#work-but').removeClass('active');
+			$('#abilities-but').addClass('active');
+			$('#projects-but').removeClass('active');
+		}
+		if (isScrolledIntoView('#projects')){
+			$('#profile-but').removeClass('active');
+			$('#work-but').removeClass('active');
+			$('#abilities-but').removeClass('active');
+			$('#projects-but').addClass('active');
+		}
+	});
+
+
+
+
+
+
+
 
