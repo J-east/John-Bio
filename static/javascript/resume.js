7af0a19944ca5c15cae8c7d801d6070e66fb9f6f
J-east <jakepevans@gmail.com>
2015-01-18 10:07:47 -0500

    Added navbar-affixed, to be affixed, fixed minor problems with jquery cdn

--- static/javascript/resume.js
+++ static/javascript/resume.js
@@ -1,9 +1,24 @@
-$.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js", function(){
+$('#navbar-affix').affix({
+  offset: {
+    top: $(window).height(),
+    bottom: function () {
+      return (this.bottom = $('.footer').outerHeight(true))
+    }
+  }
+})
 
-   alert("Script loaded and executed.");
-
-   // Use anything defined in the loaded script...
+$(function() {
+  $('a[href*=profile]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
-
-
 
