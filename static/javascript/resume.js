2baba80cd974ea7cdbb29796eacc273a202ab8bc
J-east <jakepevans@gmail.com>
2015-01-18 18:11:16 -0500

    added scroll to div functionality

--- static/javascript/resume.js
+++ static/javascript/resume.js
@@ -7,30 +7,46 @@ $('#navbar-affix').affix({
   }
 })
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+
+$('#resume-but').click(function() {
+	$('html,body').animate({
+   		scrollTop: $("#resume-body").offset().top - 30
+	});
 });
+
+$('#profile-but').click(function() {
+	$('html,body').animate({
+   		scrollTop: $("#resume-body").offset().top - 30
+	});
+});
+
+$('#resume-but').click(function() {
+	$('html,body').animate({
+   		scrollTop: $("#resume-body").offset().top - 30
+	});
+});
+
+$('#resume-but').click(function() {
+	$('html,body').animate({
+   		scrollTop: $("#resume-body").offset().top - 30
+	});
+});
+
+$('#resume-but').click(function() {
+	$('html,body').animate({
+   		scrollTop: $("#resume-body").offset().top - 30
+	});
+});
+
 
 function isScrolledIntoView(elem)
 {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+    var docViewTop = $(window).scrollTop()+30;
 
     var elemTop = $(elem).offset().top;
     var elemBottom = elemTop + $(elem).height();
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewTop));
 }
 
 $(window).scroll(function() {
