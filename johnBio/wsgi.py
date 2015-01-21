530c6767c7bebfcb2269a007d99236a66fe7b9f6
J-east <jakepevans@gmail.com>
2015-01-21 09:57:28 -0500

    added production settings

--- johnBio/wsgi.py
+++ johnBio/wsgi.py
@@ -6,9 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+import os, sys
+sys.path.append('/home/johnevans/webapps/personal_app/myproject')
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "johnBio.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "johnBio.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
